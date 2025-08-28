79f78cecdb2f31bc6a6576a6b2fb331f764a0126
Taufiq <taufiqhussain28@gmail.com>
2025-08-28 15:41:17 +0600

    herat icon functionality and call functionality added

---
+++ script.js
@@ -0,0 +1,90 @@
+// function to all heart icon
+function handleHeartIcon(){
+    const heartCount = parseInt(document.getElementById("heart-count").innerText);
+    const totalHeart = heartCount + 1;
+    document.getElementById("heart-count").innerText = totalHeart;
+}
+
+// function to call btn 
+function handleCallBtn(id1,id2){
+    const coinCount = parseInt(document.getElementById("coin-count").innerText);
+
+    if(coinCount < 20){
+        alert("You need to have at least 20 coins to make a call.")
+        return
+    }
+
+    const totalCoin = coinCount - 20;
+    document.getElementById("coin-count").innerText = totalCoin;
+    
+    const serviceName = document.getElementById(id1).innerText;
+    const serviceNumber = document.getElementById(id2).innerText;
+    alert('📞' + serviceName + ' : ' + serviceNumber);
+}
+
+// heart icon functionality
+document.getElementById("heart-icon-1").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-2").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-3").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-4").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-5").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-6").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-7").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-8").addEventListener("click", function(){
+    handleHeartIcon();
+})
+document.getElementById("heart-icon-9").addEventListener("click", function(){
+    handleHeartIcon();
+})
+
+
+// call btn functionality
+document.getElementById("call-btn-1").addEventListener("click", function(){
+    handleCallBtn("service-name-1", "service-number-1")
+})
+
+document.getElementById("call-btn-2").addEventListener("click", function(){
+    handleCallBtn("service-name-2", "service-number-2")
+})
+
+document.getElementById("call-btn-3").addEventListener("click", function(){
+    handleCallBtn("service-name-3", "service-number-3")
+})
+
+document.getElementById("call-btn-4").addEventListener("click", function(){
+    handleCallBtn("service-name-4", "service-number-4")
+})
+
+document.getElementById("call-btn-5").addEventListener("click", function(){
+    handleCallBtn("service-name-5", "service-number-5")
+})
+
+document.getElementById("call-btn-6").addEventListener("click", function(){
+    handleCallBtn("service-name-6", "service-number-6")
+})
+
+document.getElementById("call-btn-7").addEventListener("click", function(){
+    handleCallBtn("service-name-7", "service-number-7")
+})
+
+document.getElementById("call-btn-8").addEventListener("click", function(){
+    handleCallBtn("service-name-8", "service-number-8")
+})
+
+document.getElementById("call-btn-9").addEventListener("click", function(){
+    handleCallBtn("service-name-9", "service-number-9")
+})
